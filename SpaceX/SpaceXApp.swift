33e12fb0cb84fe20725145f4c5df8e9b861b1a46
heoh888 <a.khodakow@gmail.com>
2023-02-28 15:05:00 +0300

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Алексей Ходаков on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
